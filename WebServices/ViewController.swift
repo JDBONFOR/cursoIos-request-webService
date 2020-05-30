a6a5fb382bb0919c368c224fa16d383c40cc52bb
Juan Bonforti <juan@synapticlinks.com.ar>
2020-05-30 11:11:12 -0300

    Initial Commit

---
+++ WebServices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebServices
+//
+//  Created by Juan Bonforti on 30/05/2020.
+//  Copyright © 2020 Juan Bonforti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
